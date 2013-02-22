8aa260922839222e3146be6dd757eec7ef1e46a2
Bence Magyar <bence.magyar@gmail.com>
2013-02-21 21:28:18 -0500

    added user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130222021549) do
+ActiveRecord::Schema.define(:version => 20130222022704) do
 
   create_table "topics", :force => true do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
